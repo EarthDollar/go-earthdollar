8b4a84741ff93ff6684eca4c26657cedd2a20ff5
tzunami <tzunamiriverboy@gmail.com>
2016-06-14 17:36:16 -0400

    updating: eth=>ed

--- ed/gpu_mining.go
+++ ed/gpu_mining.go
@@ -1,18 +1,18 @@
-// Copyright 2014 The go-ethereum Authors
-// This file is part of the go-ethereum library.
+// Copyright 2014 The go-earthdollar Authors
+// This file is part of the go-earthdollar library.
 //
-// The go-ethereum library is free software: you can redistribute it and/or modify
+// The go-earthdollar library is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Lesser General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
 //
-// The go-ethereum library is distributed in the hope that it will be useful,
+// The go-earthdollar library is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 // GNU Lesser General Public License for more details.
 //
 // You should have received a copy of the GNU Lesser General Public License
-// along with the go-ethereum library. If not, see <http://www.gnu.org/licenses/>.
+// along with the go-earthdollar library. If not, see <http://www.gnu.org/licenses/>.
 
 // +build opencl
 
@@ -33,10 +33,10 @@ import (
 	"github.com/Earthdollar/go-earthdollar/miner"
 )
 
-func (s *Ethereum) StartMining(threads int, gpus string) error {
-	eb, err := s.Etherbase()
+func (s *Earthdollar) StartMining(threads int, gpus string) error {
+	eb, err := s.Earthbase()
 	if err != nil {
-		err = fmt.Errorf("Cannot start mining without etherbase address: %v", err)
+		err = fmt.Errorf("Cannot start mining without earthbase address: %v", err)
 		glog.V(logger.Error).Infoln(err)
 		return err
 	}
