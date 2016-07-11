97f88469ab9d61199375719ac1980163b04527ae
tzunami <tzunamiriverboy@gmail.com>
2016-07-11 00:49:11 -0400

    Beta One

--- jsre/earthdollar_js.go
+++ jsre/earthdollar_js.go
@@ -4069,7 +4069,7 @@ if (typeof Meteor !== 'undefined' && Meteor.isServer) { // jshint ignore: line
  * HttpProvider should be used to send rpc calls over http
  */
 var HttpProvider = function (host) {
-    this.host = host || 'http://localhost:8811';
+    this.host = host || 'http://localhost:8545';
 };
 
 /**
